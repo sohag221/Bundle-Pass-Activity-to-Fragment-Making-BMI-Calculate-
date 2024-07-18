f224673fb0c93ddbcf89ca466c37fe18352e8da8
sohag221 <mdratulsohag1234@gmail.com>
2024-07-19 02:36:42 +0600

    Complete ui and backend

---
+++ app/src/main/java/com/example/bmicalculator/BMI_fragment.kt
@@ -0,0 +1,37 @@
+package com.example.bmicalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.bmicalculator.databinding.FragmentBMIFragmentBinding
+
+class BMI_fragment : Fragment() {
+
+    lateinit var binding: FragmentBMIFragmentBinding
+
+    lateinit var result : TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view:View = inflater.inflate(R.layout.fragment_b_m_i_fragment, container, false)
+
+        result = view.findViewById(R.id.result)
+
+        val weight = arguments?.getInt("weight")!!.toInt()
+        val height = arguments?.getInt("height")!!.toInt()
+
+        val bmi:Double = ((weight*1000)/(height*height)).toDouble()
+        result.text = "Your BMI is : $bmi"
+
+        return view
+    }
+
+
+
+}
